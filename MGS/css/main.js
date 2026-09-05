4efb69cff0623a742b5c546a1c76c42e81bb71e1
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-06 00:27:38 +0700

    update MGS/css/main.js

--- MGS/css/main.js
+++ MGS/css/main.js
@@ -53,7 +53,7 @@ async function doJb() {
     }
 
     if (fn.setuid.invoke(0) !== -1) {
-      msgs.innerHTML = "GoldHEN is Already Loaded ...";
+      msgs.innerHTML = "GoldHEN sudah dimuat ...";
       return;
     }
 
@@ -111,10 +111,10 @@ async function doJb() {
       load_bin(bin_u8);
     }
 
-    msgs.innerHTML = "GoldHEN v2.4b18.10 Loaded ...";
+    msgs.innerHTML = "GoldHEN v2.4b18.10 Dimuat ...";
     logger.info("===END===");
   } catch (e) {
-    msgs.innerHTML = "Failed to Load! Restart Your Console ...";
+    msgs.innerHTML = "Gagal Memuat! Mulai Ulang Konsol Anda ...";
     msgs.style.color = "yellow";
   }
 }
