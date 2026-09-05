22b17b5b2acc1e40d72c610e949c9f554cc0d16b
MGstore96 <akuntvcoocaa12@gmail.com>
2026-09-06 00:20:47 +0700

    update MGS/slopkit/chain_poops.js

--- MGS/slopkit/chain_poops.js
+++ MGS/slopkit/chain_poops.js
@@ -41,14 +41,14 @@ const STOP_BEFORE_DOUBLE = params.get("stop") === "beforedouble";
 function hostOk() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "GoldHEN v2.4b18.10 Loaded ...";
+        m.innerHTML = "GoldHEN v2.4b18.10 Dimuat ...";
     }
 }
 
 function hostFail() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "Failed to Load! Restart Your Console ...";
+        m.innerHTML = "Gagal Memuat! Mulai Ulang Konsol Anda ...";
         m.style.color = "yellow";
     }
 }
